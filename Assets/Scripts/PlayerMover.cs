d1aa72ae6a097f6e826881fefff86fbcebc40ceb
Tvejzer <art2q5@gmail.com>
2023-11-29 17:23:51 +0100

    smth

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (DialogueManager.GetInstance().dialogueIsPlaying)
+        if (!movementAllowed)
         {
             return;
         }
